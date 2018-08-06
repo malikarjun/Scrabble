dde25d67ef76f8ac401619d917f56c02e06d0ba3
Mallikarjun Swamy <mswami032@gmail.com>
2018-08-06 14:53:07 +0530

    add function declarations

---
+++ src/Scrabble.java
@@ -0,0 +1,9 @@
+import javafx.util.Pair;
+
+public class Scrabble {
+    Pair<String, Integer> findHighestScore(String tiles, int condition) {
+        Pair<String, Integer> ans = new Pair<>("", 0);
+        return ans;
+    }
+
+}
---
+++ src/Utils.java
@@ -0,0 +1,15 @@
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+
+public class Utils {
+    ArrayList<Pair<String, Integer>> extractWords(String path, int n) {
+        ArrayList<Pair<String, Integer>> words = new ArrayList<Pair<String, Integer>>();
+        return words;
+    }
+
+    ArrayList<String> generateComninations(int n, String tiles) {
+        ArrayList<String> combos = new ArrayList<>();
+        return combos;
+    }
+}
