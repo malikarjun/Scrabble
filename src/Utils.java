68eccc431c497054016ed69b89e305c30e655c1a
Narmathaa L <narmathaa18@gmail.com>
2018-08-06 15:46:55 +0530

    add generateCombinations

--- src/Utils.java
+++ src/Utils.java
@@ -1,15 +1,43 @@
 import javafx.util.Pair;
-
+import java.util.Iterator;
 import java.util.ArrayList;
 
 public class Utils {
+    private static StringBuilder output = new StringBuilder();
+    public static String inputstring;
+    public static ArrayList<String> combinations = new ArrayList<String>();
+
     ArrayList<Pair<String, Integer>> extractWords(String path, int n) {
         ArrayList<Pair<String, Integer>> words = new ArrayList<Pair<String, Integer>>();
         return words;
     }
-
-    ArrayList<String> generateComninations(int n, String tiles) {
-        ArrayList<String> combos = new ArrayList<>();
-        return combos;
+    public static void combine() {
+        combine( 0 );
     }
+    private static void combine(int start ){
+        for( int i = start; i < inputstring.length(); ++i ){
+            output.append( inputstring.charAt(i) );
+            combinations.add(output.toString());
+            if ( i < inputstring.length() )
+                combine( i + 1);
+            output.setLength( output.length() - 1 );
+        }
+    }
+
+    public static ArrayList<String> generateComninations(int n, String inputString) {
+        if(inputString.length() == n) {
+            combinations.add(inputString);
+            return combinations;
+        }
+        inputstring = inputString;
+        combine();
+        Iterator itr = combinations.iterator();
+        while(itr.hasNext()) {
+            if(itr.next().toString().length() != n ) {
+                itr.remove();
+            }
+        }
+        return combinations;
+    }
+
 }
