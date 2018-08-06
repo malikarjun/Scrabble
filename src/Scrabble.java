5a9a8ed5c64f1ae6f8aa2b64a03f852ab835dd92
Mallikarjun Swamy <mswami032@gmail.com>
2018-08-06 16:21:45 +0530

    add findHighestScore

--- src/Scrabble.java
+++ src/Scrabble.java
@@ -1,8 +1,28 @@
 import javafx.util.Pair;
 
+import java.util.ArrayList;
+
 public class Scrabble {
     Pair<String, Integer> findHighestScore(String tiles, int condition) {
         Pair<String, Integer> ans = new Pair<>("", 0);
+
+
+        int score = -1;
+        String answerWord;
+        for(int i = 0; i < tiles.length(); i++) {
+            ArrayList<Pair<String, Integer>> wordList = Utils.extractWords("sowpods.txt", tiles.length() - i);
+            ArrayList<String> tileLetterCombos = Utils.generateComninations(tiles.length() - i, tiles);
+
+            for(String word: tileLetterCombos) {
+                int currScore = Utils.checkWordList(wordList, word);
+                if(score < currScore) {
+                    score = currScore;
+                    answerWord = word;
+
+                }
+
+            }
+        }
         return ans;
     }
 
--- src/Utils.java
+++ src/Utils.java
@@ -1,15 +1,37 @@
 import javafx.util.Pair;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Utils {
-    ArrayList<Pair<String, Integer>> extractWords(String path, int n) {
+    public static ArrayList<Pair<String, Integer>> extractWords(String path, int n) {
         ArrayList<Pair<String, Integer>> words = new ArrayList<Pair<String, Integer>>();
         return words;
     }
 
-    ArrayList<String> generateComninations(int n, String tiles) {
+    public static ArrayList<String> generateComninations(int n, String tiles) {
         ArrayList<String> combos = new ArrayList<>();
         return combos;
+    }
+
+    private static int getSignature(String word) {
+        return -1;
+    }
+
+    private static int computeScore(String word) {
+        int []scoreMap = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+        int score = 0;
+        for(char c: word.toCharArray()) {
+            score += scoreMap[(c - 'A')];
+        }
+        return score;
+    }
+
+    public static int checkWordList(ArrayList<Pair<String, Integer>> wordList, String word) {
+        int signature = getSignature(word);
+
+
+
+        return -1;
     }
 }
